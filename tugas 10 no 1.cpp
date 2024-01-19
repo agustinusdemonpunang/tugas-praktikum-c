a6afc8b5de4a2fead7d8d374c72550d7993075e5
agustinusdemonpunang <147039544+agustinusdemonpunang@users.noreply.github.com>
2024-01-19 16:37:22 +0700

    Add files via upload

---
+++ tugas 10 no 1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main() {
+    int A[5] = {12, 8, 17, 5, 15};
+    int B[7] = {5, 11, 7, 25, 9, 12, 14};
+    int C[12];
+    int indexC = 0;
+
+    // Menyalin nilai yang lebih besar dari 10 dari array A ke array C
+    for (int i = 0; i < 5; i++) {
+        if (A[i] > 10) {
+            C[indexC] = A[i];
+            indexC++;
+        }
+    }
+
+    // Menyalin nilai yang lebih besar dari 10 dari array B ke array C
+    for (int i = 0; i < 7; i++) {
+        if (B[i] > 10) {
+            C[indexC] = B[i];
+            indexC++;
+        }
+    }
+
+    // Menampilkan isi array C
+    std::cout << "a." << std::endl;
+    for (int i = 0; i < indexC; i++) {
+        std::cout << C[i] << std::endl;
+    }
+
+    // Menampilkan isi array C dengan format 2 baris
+    std::cout << "b." << std::endl;
+    for (int i = 0; i < indexC; i += 2) {
+        std::cout << C[i] << " " << C[i+1] << std::endl;
+    }
+
+    return 0;
+}
